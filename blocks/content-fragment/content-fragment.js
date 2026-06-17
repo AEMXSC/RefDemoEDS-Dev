9124bad0991bca28552c99e95cc3edde45cbb565
pankaj2186 <pansingh@adobe.com>
2026-06-17 10:39:24 +0530

    changed const to let

--- blocks/content-fragment/content-fragment.js
+++ blocks/content-fragment/content-fragment.js
@@ -25,7 +25,7 @@ export default async function decorate(block) {
   const ctaStyle = block.querySelector(':scope div:nth-child(5) > div')?.textContent?.trim() || 'button';
 
   block.innerHTML = '';
-  const isAuthor = isAuthorEnvironment();
+  let isAuthor = isAuthorEnvironment();
 
   // Prepare request configuration based on environment
   const requestConfig = isAuthor
